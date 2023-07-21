c8d264bfc1546c4ab7ea83fd67ff8c9a67770b01
GiovanaGama <giovana.gama.dc@gmail.com>
2023-07-21 17:28:07 -0300

    New commit

--- index.js
+++ index.js
@@ -1,35 +1,13 @@
-// async function carregarMensagem(){
-//     const response = await  axios.get('http://127.0.0.1:8000/mensagem')
-
-//     const mensagens = response.data
-
-//     const lista = document.getElementById('lista-mensagens')
-
-//     lista.innerHTML = ''
-
-//     mensagens.forEach(mensagem => {
-//         const item = document.createElement('li')
-
-//         const linha = `${mensagem.message}, ${mensagem.alexa}`
-//         item.innerText = linha
-
-//         item.appendChild(item)
-//     });
-// }
-
 function manipularMensagem() {
     const form_mensagem = document.getElementById("form-mensagem")
     const input_msg = document.getElementById("msg")
     const input_alexa = document.getElementById("alexa")
-}
-
-function sendMsg(input_msg, input_alexa, form_mensagem) {
 
     form_mensagem.onsubmit = async(event) => {
         event.preventDefault()
         const msg_mensagem = input_msg.value
         const alexa_mensagem = input_alexa.value
-        await axios.post('http://127.0.0.1:8000/mensagem', {
+        await axios.post('https://fastapideta-1-r0743913.deta.app/', {
             message: msg_mensagem,
             alexa: alexa_mensagem
         })
